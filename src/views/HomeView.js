7aa8760ac4cd9f58d488bfb9a37deb283ac018fb
Daniel Rygaard Rygaard Daniel <danryg@student.chalmers.se>
2022-03-15 00:20:24 +0100

    Added navigation buttons to the main screen

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import WideListButton from '../components/WideListButton';
 
 export function HomeView(props) {
+
+
+
+
     return (
-        <View style={styles.titleBox}>
-            <Text style={styles.titleText}>CityPop</Text>
+        <View style={styles.container}>
+            <View style={styles.titleBox}>
+                <Text style={styles.titleText}>CityPop</Text>
+            </View>
+            <View style={styles.buttonBox}>
+                <WideListButton text="SEARCH BY CITY" handler={()=>{console.log("pressed")}} />
+                <WideListButton text="SEARCH BY COUNTRY" handler={()=>{console.log("pressed")}} />
+                
+            </View>
         </View>
+
     );
+    
 }
 
+
+
 const styles = StyleSheet.create({
-    
+    container:{
+        flex:1,
+    },
     titleBox:{
+        flex:0.4,
+        justifyContent: 'center',
+    },
+    buttonBox:{
         flex:0.5,
-        justifyContent: 'center'
+        
+        alignItems:'center'
     },
     titleText:{
         textAlign:'center',
