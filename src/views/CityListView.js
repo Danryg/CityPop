87257442cbe76b7dbd1083c0a90ea142e7af4da3
Daniel Rygaard Rygaard Daniel <danryg@student.chalmers.se>
2022-03-16 00:14:10 +0100

    Create CityListView.js

---
+++ src/views/CityListView.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+function CityListView() {
+    return (
+        <View style={styles.content}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    content:{
+        flex:1,
+        backgroundColor: "red"
+    }
+})
+
+export default CityListView;
