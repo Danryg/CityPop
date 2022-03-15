67b0ab836ee8de4ece607044ed0feb52f28f73ee
Daniel Rygaard Rygaard Daniel <danryg@student.chalmers.se>
2022-03-15 21:22:39 +0100

    Changed into a Navigation container

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 import {HomeView} from './src/views/HomeView';
+import {SearchByCityView} from './src/views/SearchByCityView'
+
+import screens from './src/config/screens';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.mainModule}>
-      <HomeView/>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name={screens.Home} component={HomeView}/>
+        <Stack.Screen name={screens.SearchCityView} component={SearchByCityView}/>
+      </Stack.Navigator>
+    </NavigationContainer>
     
     
     
