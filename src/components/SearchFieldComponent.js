495ef5eae4670ced9b912756e2b7f64140ad1e4d
Daniel Rygaard Rygaard Daniel <danryg@student.chalmers.se>
2022-03-15 20:54:28 +0100

    Changed the SearchFieldComponent to a component class instead of a stateless function
    Added the state variable search,
    created a setSearchText function

--- src/components/SearchFieldComponent.js
+++ src/components/SearchFieldComponent.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import WideTextInput from './WideTextInput';
 import {View, Image, StyleSheet,TouchableHighlight} from 'react-native';
 
-export function SearchFieldComponent(props) {
-    return (
-        <View style={styles.buttonContainer}>
-            <WideTextInput value placeholder ='Enter a City '/>
-            <TouchableHighlight onPress={()=>props.searchHandler("search")} style={styles.searchButtonContainer}>
-                <Image style={styles.searchButton} source={require('../../assets/SearchIcon.png')}></Image>
-            </TouchableHighlight>
-        </View>
-    );
+
+
+
+export class SearchFieldComponent extends Component{
+    
+    state ={
+        search: 20,
+    };
+    
+    render(){
+        return (
+            <View style={styles.buttonContainer}>
+                <WideTextInput placeholder ='Enter a City' searchTextHandler={this.setSearchText}/>
+                <TouchableHighlight onPress={()=> this.props.searchHandler(this.state.search)} style={styles.searchButtonContainer}>
+                    <Image style={styles.searchButton} source={require('../../assets/SearchIcon.png')}></Image>
+                </TouchableHighlight>
+            </View>
+            
+        ); 
+    }
+
+    setSearchText = (text) =>{
+        this.setState({search: text});
+    }
+
+    
 }
+
+
 
 const styles = StyleSheet.create({
     buttonContainer:{
