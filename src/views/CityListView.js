1c36cf94116c1c7f159866158644f3b065ca7898
Daniel Rygaard Rygaard Daniel <danryg@student.chalmers.se>
2022-03-16 00:40:58 +0100

    Added all components and added some temporary city objects in the state object that will be rendered

--- src/views/CityListView.js
+++ src/views/CityListView.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet , FlatList} from 'react-native';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import CenteredTitleComponent from '../components/CenteredTitleComponent';
+import CountryListItem from '../components/CountryListItem';
 
-function CityListView() {
+
+const DATA = [
+    {
+      id: '1',
+      title: 'Paris',
+    },
+    {
+      id: '2',
+      title: 'thing2',
+    },
+    {
+      id: '3',
+      title: 'thing3',
+    },
+  ];
+
+
+const CityListView = () =>{
+    const renderItem = ({ item }) => (
+        <CountryListItem name={item.title} />
+    );
     return (
         <View style={styles.content}>
-
+            <CenteredTitleComponent title="Country name"/>
+            <View style={styles.cityList}>
+                <FlatList 
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor= {item => item.id}
+                />
+            </View>
         </View>
     );
 }
@@ -12,7 +42,12 @@ function CityListView() {
 const styles = StyleSheet.create({
     content:{
         flex:1,
-        backgroundColor: "red"
+        
+    },
+    cityList:{
+        flex:1,
+        alignItems:'center',
+
     }
 })
 
