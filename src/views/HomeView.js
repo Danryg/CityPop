3f509c551e7d44c9f8b2ab253274295cac8ffe63
Daniel Rygaard Rygaard Daniel <danryg@student.chalmers.se>
2022-03-15 21:38:55 +0100

    Commented Homeview

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,25 +2,27 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import WideListButton from '../components/WideListButton';
 
-export function HomeView(props) {
 
-
-
+/**
+ * Home page with navigation to main features
+ * @returns HomeView Component
+ */
+export function HomeView() {
 
     return (
         <View style={styles.container}>
+            {/** Centered title */}
             <View style={styles.titleBox}>
                 <Text style={styles.titleText}>CityPop</Text>
             </View>
+
+            {/** Navigation box */}
             <View style={styles.buttonBox}>
                 <WideListButton text="SEARCH BY CITY" handler={()=>{console.log("pressed")}} />
                 <WideListButton text="SEARCH BY COUNTRY" handler={()=>{console.log("pressed")}} />
-                
             </View>
         </View>
-
     );
-    
 }
 
 
