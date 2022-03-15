034f2654494bf492bced6a6ab7787d780ab8504f
Daniel Rygaard Rygaard Daniel <danryg@student.chalmers.se>
2022-03-15 13:51:02 +0100

    Created a search field component to use when the user need to search

---
+++ src/components/SearchFieldComponent.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import WideTextInput from './WideTextInput';
+import {View, Image, StyleSheet} from 'react-native';
+
+export function SearchFieldComponent(props) {
+    return (
+        <View style={styles.buttonContainer}>
+            <WideTextInput  placeholder ='Enter a City '/>
+            <View  style={styles.searchButtonContainer}>
+                <Image style={styles.searchButton} source={require('../../assets/SearchIcon.png')}></Image>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttonContainer:{
+        flex: 1,
+        alignItems: 'center'
+
+    },
+    searchButtonContainer:{
+        width: 60,
+        height: 60,
+        borderWidth: 1,
+        borderRadius: 50,
+        borderColor: "black",
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "white",
+
+        elevation: 7,
+    },
+    searchButton:{
+        width: 30,
+        height: 30,
+
+    }
+})
+
+export default SearchFieldComponent;
