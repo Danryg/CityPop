68917995952747b5e1aaeaea905c86ec833da81b
Daniel Rygaard Rygaard Daniel <danryg@student.chalmers.se>
2022-03-15 22:00:02 +0100

    Changed HomeVIew from a stateless function to a class

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import WideListButton from '../components/WideListButton';
 
@@ -7,22 +7,26 @@ import WideListButton from '../components/WideListButton';
  * Home page with navigation to main features
  * @returns HomeView Component
  */
-export function HomeView() {
+export class HomeView extends Component {
 
-    return (
-        <View style={styles.container}>
-            {/** Centered title */}
-            <View style={styles.titleBox}>
-                <Text style={styles.titleText}>CityPop</Text>
-            </View>
+    render(){
+        return (
+            <View style={styles.container}>
+                {/** Centered title */}
+                <View style={styles.titleBox}>
+                    <Text style={styles.titleText}>CityPop</Text>
+                </View>
 
-            {/** Navigation box */}
-            <View style={styles.buttonBox}>
-                <WideListButton text="SEARCH BY CITY" handler={()=>{console.log("pressed")}} />
-                <WideListButton text="SEARCH BY COUNTRY" handler={()=>{console.log("pressed")}} />
+                {/** Navigation box */}
+                <View style={styles.buttonBox}>
+                    <WideListButton text="SEARCH BY CITY" handler={()=>{console.log("pressed")}} />
+                    <WideListButton text="SEARCH BY COUNTRY" handler={()=>{console.log("pressed")}} />
+                </View>
             </View>
-        </View>
-    );
+        );
+    }
+
+
 }
 
 
