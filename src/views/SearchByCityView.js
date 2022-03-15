a56504cc39fa0a1c886f70d1972883d8e2fe6a77
Daniel Rygaard Rygaard Daniel <danryg@student.chalmers.se>
2022-03-15 21:36:52 +0100

    Commented SearchByCityView

--- src/views/SearchByCityView.js
+++ src/views/SearchByCityView.js
@@ -4,16 +4,32 @@ import CenteredTitleComponent from '../components/CenteredTitleComponent';
 import {SearchFieldComponent} from '../components/SearchFieldComponent';
 
 
+/**
+ * This view represents the search for city feature, where the user will be able to 
+ * search for a city and move into a detailed view of that city
+ * 
+ * @param {*title The title for the view} props 
+ * @param {*searchHandler(search) function that is called when searchbutton is pressed} props 
+ * @returns SearchByCityView component
+ */
 export function SearchByCityView(props) {
     return (
         <View style={styles.container}>
+            {/* Top component with back button */}
             <View style={styles.top}>
+                {/** Back button */}
                 <TouchableWithoutFeedback onPress={()=> console.log("Back")}>
                     <Image style={styles.arrow} source={require('../../assets/Arrow.jpg')} ></Image>
                 </TouchableWithoutFeedback>
+
+                {/** App title */}
                 <Text style={styles.appTitle}>CityPop</Text>
             </View>
+
+            {/** Centered main title */}
             <CenteredTitleComponent title={props.title}/>
+
+            {/** Search component */}
             <SearchFieldComponent style={styles.buttonContainer} searchHandler={(search) => props.searchHandler(search)}/>
         </View>
     );
